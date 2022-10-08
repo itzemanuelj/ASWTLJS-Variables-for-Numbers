891035c9e9e75a4c5d51c603ef1d2c9480654aa5
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-08 11:55:59 -0400

    mvp commit for variables for numbers

---
+++ script.js
@@ -0,0 +1,83 @@
+// You can assign a string to a variable. You can also assign a ______.
+var numbers;
+
+// If a number is enclosed in quotes, it's a ______.
+var string;
+
+// What kind of operation can be done on a number variable that, in one particular case, can't be done on a string variable? Answer with a 4-letter word. (It would be a five-letter word in British usage.)
+var math;
+
+// What is the value of orderTotal?
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal);
+//110
+
+var niceBigNumber = 100000000;
+
+// In a single statement, declare the variable numberAsString and assign the number 3 to it as a string.
+var numberAsString = "3";
+
+// Assign to the variable caseQty, which has already been declared, the value 144.
+var caseQty = 144;
+
+// In a single statement declare a variable and assign a number to it. (You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to.)
+
+var topScore = 1;
+
+// In a single statement declare a variable and assign to it the sum of 2 other variables. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+var topScore = cost + time;
+
+// Rewrite this statement so the variable can be used in a math operation.
+var num = "9";
+var num = 9;
+
+
+// Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+var sum = 10 + 2;
+
+// In one statement declare a variable. In a second statement assign it the sum of 2 numbers. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+var varyy;
+varry = 1 + 10;
+
+// Time goal: 20 seconds. (The timer starts when you click in the answer field.) In a single statement declare a variable and assign a number to it. You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to. (The timer starts when you click in the answer field.)
+
+var num = 10;
+
+// Time goal: 25 seconds. In a single statement declare a variable and assign to it the sum of 2 other variables. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+var sum = cost + total;
+
+// Time goal: 30 seconds. Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+var twoSum = 1 + 2;
+
+// Time goal: 30 seconds. In one statement declare a variable. In a second statement assign it the sum of 2 numbers. (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+
+var declareMe;
+declareMe = 1 + 2;
+
+// Time goal: 35 seconds. In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+
+var declareMe2 = 1;
+declareMe2 = declareMe2 + 1;
+
+// Declare a variable and assign it a number. Then code an alert, specifying the variable, not the number, as the message. Click the Run It button to run your code live. Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+var total = 10;
+alert(total);
+
+// Declare a variable and assign it a number.
+// Double the variable's value by adding the variable to itself.
+// Display the variable's value in an alert, specifying the variable, not the number, as the message.
+// Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+var cost = 10;
+cost = cost + 2;
+alert(cost);
+//20
